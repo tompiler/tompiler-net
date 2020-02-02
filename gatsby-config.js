1fade0d78f547c4d70db008e256e43e4189ffb39
tompiler <tomlynch3091341@googlemail.com>
2020-02-02 18:51:08 +0000

    made it my own a bit

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,5 +5,57 @@
  */
 
 module.exports = {
-  /* Your site config here */
+  siteMetadata: {
+    title: "tompiler",
+    author: "Andrew Mead",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-fonts",
+      options: {
+        fonts: [
+          "Montserrat sans-serif",
+          "Open Sans sans-serif",
+          "source sans pro:300,400,400i,700", // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
+    {
+      resolve: "gatsby-plugin-prefetch-google-fonts",
+      options: {
+        fonts: [
+          {
+            family: "Open Sans",
+            variants: ["400", "700"],
+          },
+        ],
+      },
+    },
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false,
+              ignoreFileExtensions: [],
+            },
+          },
+        ],
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <Layout>
+      <h1>Hello.</h1>
+      <h2>I'm tom, a technologist living in london.</h2>
+      <h2>This is a place to keep the things I make. </h2>
+      <ul>
+        <li>Articles</li>
+        <li>Tutorials</li>
+      </ul>
+      <br></br>
+      <p>
+        Have a question? <Link to="/contact">Contact me.</Link>
+      </p>
+    </Layout>
+  )
+}
+
+export default IndexPage
