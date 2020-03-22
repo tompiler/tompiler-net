f80da4d33becbaa0b0dc8d5cbbefd996401ca99d
tompiler <tomlynch3091341@googlemail.com>
2020-03-22 18:16:41 +0000

    updated mission statement

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -13,23 +13,37 @@ const Mission = () => {
           from time to time.
           <br />
           <br />
-          As I write this, we're in what apparently are the early days of the
-          Covid-19 pandemic. It's the first week of isolation, and the
-          government here in the UK have closed schools, requested that pubs and
-          restaurants must close in an effort to relieve the pressure expected
-          on the NHS in the coming weeks and months. <br />
+          I have a few ideas for articles at the moment, all dancing around in
+          my head and colliding with each other when I'm supposed to be
+          concentrating on something more important.
           <br />
-          We're facing much more time indoors than expected this year. I want to
-          create something positive from what will otherwise be a regressive
-          period for humanity around the globe.
+          <br />
+          Its a strange time.
+          <br />
+          <br />
+          Currently, we're in what are apparently the early days of the Covid-19
+          pandemic. Drastic measures are being taken by governments across the
+          globe to reduce the pressure on health services. <br />
+          <br />
+          We're now facing much more time indoors than expected this year. In
+          that time, I want to create something positive from what will
+          otherwise be a regressive period for humanity around the globe.
           <br />
           <br />
           The articles I hope to write will tell stories, illustrated with
-          graphics, and created using open source data and technology.
+          graphics, and created using open source data and technology. Themes
+          will vary a lot, and will be covered with varying depth.
           <br />
           <br />
           I'll also be making some tutorials along the way for anyone
           interested.
+          <br />
+          <br />
+          <br />
+          Tom Lynch
+          <br />
+          <br />
+          March 19th, 2020
         </div>
       </div>
     </div>
