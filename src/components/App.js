44ae919308f0451d9fe41e40ea6b648e28f19f75
tompiler <tomlynch3091341@googlemail.com>
2020-03-29 11:17:50 +0100

    added App.js for complete structure

---
+++ src/components/App.js
@@ -0,0 +1,50 @@
+import React from "react"
+import Main from "./components/Main/Main"
+import Toolbar from "./components/Toolbar/Toolbar"
+import SideDrawer from "./components/SideDrawer/SideDrawer"
+import Backdrop from "./components/Backdrop/Backdrop"
+
+class App extends React.Component {
+  state = {
+    sideDrawerOpen: false,
+    route: "tompiler",
+  }
+
+  routeHandler = route => {
+    console.log(route)
+    this.setState(() => {
+      return { route: route }
+    })
+  }
+
+  drawerToggleClickHandler = () => {
+    this.setState(prevState => {
+      return { sideDrawerOpen: !prevState.sideDrawerOpen }
+    })
+  }
+
+  backdropClickHandler = () => {
+    this.setState({ sideDrawerOpen: false })
+  }
+  render() {
+    let backdrop
+
+    if (this.state.sideDrawerOpen) {
+      backdrop = <Backdrop click={this.backdropClickHandler} />
+    }
+
+    return (
+      <div className="app">
+        <Toolbar
+          drawerClickHandler={this.drawerToggleClickHandler}
+          routeHandler={this.routeHandler}
+        />
+        <SideDrawer show={this.state.sideDrawerOpen} />
+        {backdrop}
+        <Main route={this.state.route} routeHandler={this.routeHandler} />
+      </div>
+    )
+  }
+}
+
+export default App
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,52 +1,10 @@
 import React from "react"
-import Main from "../components/Main/Main"
-import Toolbar from "../components/Toolbar/Toolbar"
-import SideDrawer from "../components/SideDrawer/SideDrawer"
-import Backdrop from "../components/Backdrop/Backdrop"
+import App from "../App.js"
 
 import "../styles/index.scss"
 
-class IndexPage extends React.Component {
-  state = {
-    sideDrawerOpen: false,
-    route: "tompiler",
-  }
-
-  routeHandler = route => {
-    console.log(route)
-    this.setState(() => {
-      return { route: route }
-    })
-  }
-
-  drawerToggleClickHandler = () => {
-    this.setState(prevState => {
-      return { sideDrawerOpen: !prevState.sideDrawerOpen }
-    })
-  }
-
-  backdropClickHandler = () => {
-    this.setState({ sideDrawerOpen: false })
-  }
-  render() {
-    let backdrop
-
-    if (this.state.sideDrawerOpen) {
-      backdrop = <Backdrop click={this.backdropClickHandler} />
-    }
-
-    return (
-      <div className="app">
-        <Toolbar
-          drawerClickHandler={this.drawerToggleClickHandler}
-          routeHandler={this.routeHandler}
-        />
-        <SideDrawer show={this.state.sideDrawerOpen} />
-        {backdrop}
-        <Main route={this.state.route} routeHandler={this.routeHandler} />
-      </div>
-    )
-  }
+const IndexPage = () => {
+  return <App />
 }
 
 export default IndexPage
