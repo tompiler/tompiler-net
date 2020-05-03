abdc86d52f24378254d207d82e794eb7e146be43
tompiler <tomlynch3091341@googlemail.com>
2020-05-03 15:14:54 +0100

    basic animated line

--- src/components/Content/CV/CV.js
+++ src/components/Content/CV/CV.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { useSpring, animated } from "react-spring"
 
 import styled from "styled-components"
@@ -15,7 +15,24 @@ const ContentContainer = styled("div")`
 
 const CV = ({ mount, transitionStatus, exit, entry, location, children }) => {
   const route = location.pathname.substr(1)
-  const props = useSpring({
+
+  const [open, toggle] = useState(false)
+
+  const opacityLineProps = useSpring({
+    from: { opacity: 0 },
+    to: {
+      opacity: open ? 1 : 0,
+    },
+    config: {
+      //   duration: 1000,
+      mass: 1,
+      tension: 120,
+      friction: 15,
+    },
+  })
+  const lineProps = useSpring({ to: { y: open ? 500 : 10 }, from: { y: 10 } })
+
+  const opacityProps = useSpring({
     from: { opacity: 0 },
     to: {
       opacity: mount && route === "cv" ? 1 : 0,
@@ -30,8 +47,27 @@ const CV = ({ mount, transitionStatus, exit, entry, location, children }) => {
 
   return (
     <ContentContainer>
-      <animated.div style={{ fontSize: "43px", opacity: props.opacity }}>
+      <button onClick={() => toggle(!open)}>TOGGLE ME</button>
+      <animated.div style={{ fontSize: "43px", opacity: opacityProps.opacity }}>
         <div>Hello</div>
+        <animated.svg
+          height="600"
+          width="100"
+          // strokeDashoffset={lineProps.x}
+        >
+          <g>
+            <rect height="600" width="100" fill="#636774"></rect>
+            <animated.line
+              x1="50"
+              y1="10"
+              x2="50"
+              y2={lineProps.y}
+              stroke="#d4cac1"
+              strokeWidth="2"
+              // style={{ opacity: opacityLineProps.opacity }}
+            />
+          </g>
+        </animated.svg>
       </animated.div>
     </ContentContainer>
   )
