a16edb529e577005bd3c215479ec726e0a4a86f7
tompiler <tomlynch3091341@googlemail.com>
2020-05-13 13:28:56 +0100

    added aims to markdown

--- src/components/Content/Aims/Aims.js
+++ src/components/Content/Aims/Aims.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { graphql, useStaticQuery } from "gatsby"
+
 import styled from "styled-components"
 
 const AimsItem = styled("div")`
@@ -23,41 +25,24 @@ const AimsItem = styled("div")`
 `
 
 const Aims = () => {
+  const data = useStaticQuery(graphql`
+    query AimsQuery {
+      markdownRemark(fields: { slug: { eq: "aims" } }) {
+        frontmatter {
+          title
+          date
+        }
+        html
+      }
+    }
+  `)
   return (
     <AimsItem>
-      I've made this site primarily as a place to host bits of work I do from
-      time to time.
-      <br />
-      <br />
-      I have a few ideas for articles at the moment, all dancing around in my
-      head and colliding with each other when I'm supposed to be concentrating
-      on something more important.
-      <br />
-      <br />
-      Its a strange time.
-      <br />
-      <br />
-      Currently, we're in what are apparently the early days of the Covid-19
-      pandemic. Drastic measures are being taken by governments across the globe
-      to reduce the pressure on health services. <br />
-      <br />
-      We're now facing much more time indoors than expected this year. In that
-      time, I want to create something positive during what will otherwise feel
-      like a chaotic and concerning period for humanity as a whole.
-      <br />
-      <br />
-      The articles I hope to write will tell stories, illustrated with graphics,
-      and created using open source data and technology. Themes will vary a lot,
-      and will be covered with varying depth.
-      <br />
-      <br />
-      I'll also be making some tutorials along the way for anyone interested.
-      <br />
-      <br />
-      Tom Lynch
-      <br />
-      <br />
-      March 19th, 2020
+      <div
+        className="boobieblahblah"
+        dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }}
+      ></div>
+      <p>{data.markdownRemark.frontmatter.date}</p>
     </AimsItem>
   )
 }
---
+++ src/posts/aims.md
@@ -0,0 +1,29 @@
+---
+title: "Aims"
+date: "March 19th, 2020"
+---
+
+I've made this site primarily as a place to host bits of work I do from
+time to time.
+
+I have a few ideas for articles at the moment, all dancing around in my
+head and colliding with each other when I'm supposed to be concentrating
+on something more important.
+
+Its a strange time.
+
+Currently, we're in what are apparently the early days of the Covid-19
+pandemic. Drastic measures are being taken by governments across the globe
+to reduce the pressure on health services. <br />
+
+We're now facing much more time indoors than expected this year. In that
+time, I want to create something positive during what will otherwise feel
+like a chaotic and concerning period for humanity as a whole.
+
+The articles I hope to write will tell stories, illustrated with graphics,
+and created using open source data and technology. Themes will vary a lot,
+and will be covered with varying depth.
+
+I'll also be making some tutorials along the way for anyone interested.
+
+Tom Lynch
