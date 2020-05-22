96f4cbfae96965085e483b106659f93d39a4b7da
tompiler <tomlynch3091341@googlemail.com>
2020-05-22 16:52:01 +0100

    minor changes

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -23,6 +23,7 @@ const SEO = ({ title, description, image, article }) => {
   }
   return (
     <Helmet title={seo.title} titleTemplate={titleTemplate}>
+      <html lang="en" />
       <meta name="description" content={seo.description} />
       <meta name="image" content={seo.image} />
       {seo.url && <meta property="og:url" content={seo.url} />}
